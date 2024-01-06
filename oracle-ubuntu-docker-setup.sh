c7902a2e3cf0a01ddd8ea08c1d5d06fa3dfe3b54
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-01-06 23:39:31 +0700

    Update oracle-ubuntu-docker-setup.sh

--- oracle-ubuntu-docker-setup.sh
+++ oracle-ubuntu-docker-setup.sh
@@ -1,26 +1,43 @@
+#!/bin/bash
+
+# cập nhập OS
 sudo apt update && sudo apt upgrade -y
 
+# set locale
 locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
+# Tắt Firewall
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
 
+# Chỉnh về múi giờ Việt Nam
 timedatectl set-timezone Asia/Ho_Chi_Minh
+
+# Tạo swap 4GB RAM
 sudo fallocate -l 4G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 cat <<EOF > /etc/sysctl.d/99-xs-swappiness.conf
 vm.swappiness=10
 EOF
 
+# Enable TCP BBR congestion control
+cat <<EOF > /etc/sysctl.conf
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+# Cài đặt docker và docker-compose
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $(whoami)
 sudo systemctl start docker
 sudo systemctl enable docker
 apt install docker-compose -y
 
+# Cài đặt Rclone
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
+rclone version
 
 # chỉ dành riêng cho Oracle VPS (không cần cài đặt nếu dùng VPS hãng khác)
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/NeverIdle-Oracle/master/VM.Standard.E2.1.Micro.sh -O /usr/local/bin/bypass_oracle.sh
